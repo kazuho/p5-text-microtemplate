cf934b5ab9943938d8acb89061fdeb111e5bfd3d
kazuho <kazuho@d0d07461-0603-4401-acd4-de1884942a52>
2008-12-16 10:12:35 +0000

    add cache
    git-svn-id: http://svn.coderepos.org/share/lang/perl/Text-MicroTemplate/trunk@26872 d0d07461-0603-4401-acd4-de1884942a52

--- lib/Text/MicroTemplate/File.pm
+++ lib/Text/MicroTemplate/File.pm
@@ -4,6 +4,8 @@ use strict;
 use warnings;
 use Text::MicroTemplate;
 
+use Carp qw(croak);
+
 our @ISA = qw(Text::MicroTemplate);
 
 sub new {
@@ -14,6 +16,8 @@ sub new {
     unless (ref $self->{include_path}) {
         $self->{include_path} = [ $self->{include_path} ];
     }
+    $self->{use_cache} ||= 0;
+    $self->{cache} = {};  # file => { mtime, sub }
     $self;
 }
 
@@ -24,14 +28,39 @@ sub open_layer {
     $self->{open_layer};
 }
 
+sub use_cache {
+    my $self = shift;
+    $self->{use_cache} = $_[0]
+        if @_;
+    $self->{use_cache};
+}
+
 sub build_file {
     my ($self, $file) = @_;
+    # return cached entry
+    if ($self->{use_cache} == 2) {
+        if (my $e = $self->{cache}->{$file}) {
+            return $e->[1];
+        }
+    }
+    # iterate
     foreach my $path (@{$self->{include_path}}) {
-        if (open my $fh, "<$self->{open_layer}", $path . '/' . $file) {
+        my $filepath = $path . '/' . $file;
+        if (my @st = stat $filepath) {
+            if (my $e = $self->{cache}->{$file}) {
+                return $e->[1]
+                    if $st[9] == $e->[0];
+            }
+            open my $fh, "<$self->{open_layer}", $filepath
+                or croak "failed to open:$filepath:$!";
             my $src = do { local $/; join '', <$fh> };
             close $fh;
             $self->parse($src);
-            return $self->build();
+            $self->{cache}->{$file} = [
+                $st[9],
+                my $f = $self->build(),
+            ];
+            return $f;
         }
     }
     die "could not find template file: $file\n";
--- t/07-file.t
+++ t/07-file.t
@@ -1,11 +1,13 @@
 use strict;
 use warnings;
-use Test::More tests => 7;
+use Test::More tests => 12;
+use File::Temp qw(tempdir);
 
 BEGIN {
     use_ok('Text::MicroTemplate::File');
 };
 
+# simple test
 do {
     my $mtf = Text::MicroTemplate::File->new(
         include_path => 't/07-file',
@@ -15,6 +17,7 @@ do {
     is $mtf->render_file('package.mt')->as_string, "main\n", 'default package';
 };
 
+# package name
 do {
     my $mtf = Text::MicroTemplate::File->new(
         include_path => 't/07-file',
@@ -25,3 +28,29 @@ do {
     is $mtf->render_file('package.mt')->as_string, "foo\n", 'package';
 };
 
+# cache
+do {
+    my $dir = tempdir(CLEANUP => 1);
+    my $rewrite = sub {
+        open my $fh, '>', "$dir/t.mt"
+            or die "cannot open $dir/t.mt:$!";
+        print $fh @_;
+        close $fh;
+    };
+    my $mtf =Text::MicroTemplate::File->new(
+        include_path => $dir,
+        use_cache    => 1,
+    );
+    $rewrite->(1);
+    is $mtf->render_file('t.mt')->as_string, '1', 'cache=1 read';
+    is $mtf->render_file('t.mt')->as_string, '1', 'cache=1 retry';
+    sleep 2;
+    $rewrite->(2);
+    is $mtf->render_file('t.mt')->as_string, '2', 'cache=1 update';
+    is $mtf->render_file('t.mt')->as_string, '2', 'cache=1 update retry';
+    # set mode to 2 and remove
+    $mtf->use_cache(2);
+    unlink "$dir/t.mt"
+        or die "failed to remove $dir/t.mt:$!";
+    is $mtf->render_file('t.mt')->as_string, '2', 'cache=2 read after cached';
+};
